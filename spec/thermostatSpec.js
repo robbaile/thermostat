ac9867eb4e7dda2ed00e6a7ae577796e308f368c
Rob Baile <robbaile12@outlook.com>
2019-11-11 14:48:37 +0000

    added test for limiting temperature

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -11,4 +11,21 @@ describe("Plane", function() {
     it('should have a random flight number generated', () => {
         expect(thermostat.getCurrentTemperature()).toEqual(20)
     });
+
+    it('should raise temperature by one', () => {
+        thermostat.up()
+        expect(thermostat.getCurrentTemperature()).toEqual(21)
+    });
+
+    it('should decrease temperature by one', () => {
+        thermostat.down()
+        expect(thermostat.getCurrentTemperature()).toEqual(19)
+    });
+
+    it('should only go down to 10 degress', () => {
+        for (let i = 0; i < 15; i++) {
+            thermostat.down();
+        };
+        expect(thermostat.getCurrentTemperature()).toEqual(10)
+    });
 });
